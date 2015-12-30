3a9c1733374cada703b5b6e2ff13024e7c386b97
Walter Rodríguez <Walter Rodrguez>
2015-12-30 02:11:11 -0500

    Subida

--- js/codigo.js
+++ js/codigo.js
@@ -1,4 +1,7 @@
 $(document).ready(function(e) {	
+
+	/*Banner de Imagenes con animacion, agrega un <div> con wrapInner, captura la altura del img:first-child y lo ultiliza 		    para uzarlo como la altura del #contenido_banner;*/
+	
 	$("#contenido_banner img:first-child").load(function(e) {
 		var intervalo;
 		//379
@@ -27,7 +30,7 @@ $(document).ready(function(e) {
     });
 	
 	//Aparece el logo en 3 segundos y dandole clcik encima del logo desaparece y cambia de color y contenido al <h1>  
-	$("#logo").delay(3000).show("slow").click(function(e) {
+	$("#logo").delay(5000).show("slow").click(function(e) {
         $(this).hide("slow")
 		$("h1").html("Cambio de Titulo y de color").css("color", "#00426C");
     });
@@ -40,5 +43,39 @@ $(document).ready(function(e) {
   .css("background","url(img/icon_facebook.png)")
   .css("background-repeat","no-repeat")
   .css("padding-left","25px")
-	
+  
+  
+    var art_top = $("#articulos").offset().top;  // articulos top
+	var art_left = $("#articulos").offset().left; // carticulos left
+	var art_height = $("#articulos").height();   // articulos height
+	var art_width = $("#articulos").width();     // articulos width
+	var height_bird = $("#bird_rojo").height(); // muñeco height
+    var width_bird  = $("#bird_rojo").width(); // muñeco width   	 
+	var sonido1 = new Audio("media/angry.mp3");
+	var sonido2 = new Audio("media/pig.mp3");
+   setTimeout(mostrar_rojo,1000);
+   setTimeout(mostrar_celeste,3000);
+
+   
+   function mostrar_rojo(){ 	 
+   	 var randomtop = art_top + Math.floor((Math.random()* (art_height - height_bird))+1);
+     var randomleft = art_left + Math.floor((Math.random()* (art_width - width_bird))+1);
+	sonido1.play();
+	   	$("#bird_rojo").css("top",randomtop + "px").css("left",randomleft + "px").css("display","block")
+		.click(function(e) {
+            $(this).css("display","none");
+			sonido2.play();
+        });
+	   }
+   function mostrar_celeste(){
+	 var randomtop = art_top + Math.floor((Math.random()* (art_height - height_bird))+1);
+     var randomleft = art_left + Math.floor((Math.random()* (art_width - width_bird))+1);
+	sonido1.play();
+	   	   	$("#bird_celeste").css("top",randomtop + "px").css("left",randomleft + "px").css("display","block")
+		.click(function(e) {
+            $(this).css("display","none");
+			sonido2.play();
+        });
+	   }
+
 });
